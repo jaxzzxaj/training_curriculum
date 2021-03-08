8d0bda980eb315563673709eeb61c7680f6142b6
jaxzzxaj <programyourlife1020@gmail.com>
2021-03-08 22:19:12 +0900

    ハッシュをシンボルへ

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -44,8 +44,9 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wdays[wday_num], plans: today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: week_days[wday_num], plans: today_plans}
       @week_days.push(days)
     end
   end
 end
+
